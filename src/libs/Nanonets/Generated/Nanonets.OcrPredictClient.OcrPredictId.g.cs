60121cf7c6d259371307511c2888ae3014820b0a
Konstantin S. <havendv@gmail.com>
2026-04-03 09:12:47 +0400

    feat: Updated OpenAPI spec (#4)
    Co-authored-by: github-actions[bot] <dependabot@bot.com>

--- src/libs/Nanonets/Generated/Nanonets.OcrPredictClient.OcrPredictId.g.cs
+++ src/libs/Nanonets/Generated/Nanonets.OcrPredictClient.OcrPredictId.g.cs
@@ -240,7 +240,6 @@ namespace Nanonets
                 try
                 {
                     __response.EnsureSuccessStatusCode();
-
                     var __content = await __response.Content.ReadAsStringAsync(
 #if NET5_0_OR_GREATER
                         cancellationToken
