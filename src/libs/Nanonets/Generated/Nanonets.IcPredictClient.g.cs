701530add2bb545ee65a062a9bbfea0022415f5c
Konstantin S. <havendv@gmail.com>
2026-09-13 20:02:17 +0400

    feat: Updated OpenAPI spec (#41)
    Co-authored-by: github-actions[bot] <dependabot@bot.com>

--- src/libs/Nanonets/Generated/Nanonets.IcPredictClient.g.cs
+++ src/libs/Nanonets/Generated/Nanonets.IcPredictClient.g.cs
@@ -35,7 +35,7 @@ namespace Nanonets
         /// <inheritdoc/>
         public global::Nanonets.AutoSDKClientOptions Options { get; }
 
-        internal global::System.Lazy<global::System.Text.Json.Serialization.JsonSerializerContext> JsonSerializerContextProvider { get; set; } = new(() => global::Nanonets.SourceGenerationContext.Default);
+        internal global::System.Lazy<global::System.Text.Json.Serialization.JsonSerializerContext> JsonSerializerContextProvider { get; set; } = new(() => global::Nanonets.IcPredictSourceGenerationContext.Default);
 
         /// <summary>
         ///
